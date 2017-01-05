896bbffff18345d6cff90a580e2c3a3a3cf7959c
Salar Abbasi <salarabbasi94@gmail.com>
2017-01-05 16:47:56 +0330

    Implement navigation

---
+++ app/src/main/java/com/example/salar/makeathon/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.salar.makeathon;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class SplashScreen extends AppCompatActivity {
+    Button signUp;
+    Button signIn;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        signIn=(Button)findViewById(R.id.activity_splash_sign_in);
+        signUp=(Button)findViewById(R.id.activity_splash_sign_up);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SplashScreen.this,SignIn.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
